578c966426371530440a5ddf30e030ab831deb5d
Diego Miranda <dleon-msalazar@hotmail.com>
2021-09-01 16:41:59 -0500

    T_T_T_T
    UU

--- modules/keypoint_detector.py
+++ modules/keypoint_detector.py
@@ -88,8 +88,8 @@ class KPDetector_Unet_3(nn.Module):
         self.predictor = Unet_3_Plus(block_expansion, in_features=num_channels,
                                    max_features=max_features, num_blocks=num_blocks)
 
-        self.kp = nn.Conv2d(in_channels=1
-                            #in_channels=self.predictor.out_filters
+        self.kp = nn.Conv2d(#in_channels=1
+                            in_channels=self.predictor.out_filters
                             , out_channels=num_kp, kernel_size=(32, 32),
                             padding=pad)
 
