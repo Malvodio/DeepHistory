77b720162fad19b105a529f0930be87fbbf14cd2
Diego Miranda <dleon-msalazar@hotmail.com>
2021-09-13 10:35:52 -0500

    Partitioning new method
    a

--- frames_dataset.py
+++ frames_dataset.py
@@ -51,7 +51,6 @@ def read_video(name, frame_shape):
 
     return video_array
 
-
 class FramesDataset(Dataset):
     """
     Dataset of videos, each video can be represented as:
@@ -137,7 +136,6 @@ class FramesDataset(Dataset):
 
         return out
 
-
 class DatasetRepeater(Dataset):
     """
     Pass several times over the same dataset for better i/o performance
@@ -152,7 +150,6 @@ class DatasetRepeater(Dataset):
 
     def __getitem__(self, idx):
         return self.dataset[idx % self.dataset.__len__()]
-
 
 class PairedDataset(Dataset):
     """
@@ -195,3 +192,91 @@ class PairedDataset(Dataset):
         second = {'source_' + key: value for key, value in second.items()}
 
         return {**first, **second}
+
+class FramesDatasetPartitioning(Dataset):
+    """
+    Dataset of videos, each video can be represented as:
+      - an image of concatenated frames
+      - '.mp4' or '.gif'
+      - folder with all frames
+    """
+
+    def __init__(self, root_dir, frame_shape=(256, 256, 3), id_sampling=False, is_train=True,
+                 random_seed=0, pairs_list=None, augmentation_params=None,batches_list=None):
+        self.root_dir = root_dir
+        self.videos = os.listdir(root_dir)
+        self.frame_shape = tuple(frame_shape)
+        self.pairs_list = pairs_list
+        self.id_sampling = id_sampling
+        if os.path.exists(os.path.join(root_dir, 'train')):
+            assert os.path.exists(os.path.join(root_dir, 'test'))
+            print("Use predefined train-test split.")
+            if id_sampling:
+                train_videos = {os.path.basename(video).split('#')[0] for video in
+                                os.listdir(os.path.join(root_dir, 'train'))}
+                train_videos = list(train_videos)
+            else:
+                train_videos = os.listdir(os.path.join(root_dir, 'train'))
+            test_videos = os.listdir(os.path.join(root_dir, 'test'))
+            self.root_dir = os.path.join(self.root_dir, 'train' if is_train else 'test')
+        else:
+            print("Use random train-test split.")
+            train_videos, test_videos = train_test_split(self.videos, random_state=random_seed, test_size=0.2)
+
+        if is_train:
+            if batches_list is not None:
+                self.videos = train_videos[batches_list]
+            else:
+                self.videos = train_videos
+        else:
+            self.videos = test_videos
+
+        self.is_train = is_train
+
+        if self.is_train:
+            self.transform = AllAugmentationTransform(**augmentation_params)
+        else:
+            self.transform = None
+
+    def __len__(self):
+        return len(self.videos)
+
+    def __getitem__(self, idx):
+        if self.is_train and self.id_sampling:
+            name = self.videos[idx]
+            path = np.random.choice(glob.glob(os.path.join(self.root_dir, name + '*.mp4')))
+        else:
+            name = self.videos[idx]
+            path = os.path.join(self.root_dir, name)
+
+        video_name = os.path.basename(path)
+
+        if self.is_train and os.path.isdir(path):
+            frames = os.listdir(path)
+            num_frames = len(frames)
+            frame_idx = np.sort(np.random.choice(num_frames, replace=True, size=2))
+            video_array = [img_as_float32(io.imread(os.path.join(path, frames[idx]))) for idx in frame_idx]
+        else:
+            video_array = read_video(path, frame_shape=self.frame_shape)
+            num_frames = len(video_array)
+            frame_idx = np.sort(np.random.choice(num_frames, replace=True, size=2)) if self.is_train else range(
+                num_frames)
+            video_array = video_array[frame_idx]
+
+        if self.transform is not None:
+            video_array = self.transform(video_array)
+
+        out = {}
+        if self.is_train:
+            source = np.array(video_array[0], dtype='float32')
+            driving = np.array(video_array[1], dtype='float32')
+
+            out['driving'] = driving.transpose((2, 0, 1))
+            out['source'] = source.transpose((2, 0, 1))
+        else:
+            video = np.array(video_array, dtype='float32')
+            out['video'] = video.transpose((3, 0, 1, 2))
+
+        out['name'] = video_name
+
+        return out
